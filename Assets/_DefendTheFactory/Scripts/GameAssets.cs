8fb9bf0a571ec9aa3db6b2483f689a2957b91332
Propethunio <kuba.p.mazur@gmail.com>
2024-07-11 18:35:13 +0200

    Init

---
+++ Assets/_DefendTheFactory/Scripts/GameAssets.cs
@@ -0,0 +1,92 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+ 
+using UnityEngine;
+using System.Reflection;
+
+public class GameAssets : MonoBehaviour {
+    
+    private static GameAssets _i;
+
+    public static GameAssets i {
+        get {
+            if (_i == null) _i = Instantiate(Resources.Load<GameAssets>("GameAssets"));
+            return _i;
+        }
+    }
+
+    private void Awake() {
+        _i = this;
+    }
+
+
+
+    [System.Serializable]
+    public class PlacedObjectTypeSO_Refs {
+
+        public PlacedObjectTypeSO conveyorBelt;
+        public PlacedObjectTypeSO miningMachine;
+        public PlacedObjectTypeSO smelter;
+        public PlacedObjectTypeSO grabber;
+        public PlacedObjectTypeSO assembler;
+        public PlacedObjectTypeSO storage;
+
+    }
+
+    public PlacedObjectTypeSO_Refs placedObjectTypeSO_Refs;
+
+
+
+    [System.Serializable]
+    public class ItemSO_Refs {
+
+        public ItemSO ironOre;
+        public ItemSO goldOre;
+        public ItemSO ironIngot;
+        public ItemSO goldIngot;
+        public ItemSO computer;
+        public ItemSO copperOre;
+        public ItemSO copperIngot;
+        public ItemSO microchip;
+
+        public ItemSO any;
+        public ItemSO none;
+    }
+
+
+    public ItemSO_Refs itemSO_Refs;
+
+
+
+    [System.Serializable]
+    public class ItemRecipeSO_Refs {
+
+        public ItemRecipeSO ironIngot;
+        public ItemRecipeSO goldIngot;
+        public ItemRecipeSO computer;
+        public ItemRecipeSO microchip;
+        public ItemRecipeSO copperIngot;
+    }
+
+
+    public ItemRecipeSO_Refs itemRecipeSO_Refs;
+
+
+    public Transform pfWorldItem;
+    public Transform pfBeltDebugVisualNode;
+    public Transform pfBeltDebugVisualLine;
+
+    public Transform fxBuildingPlaced;
+
+    public Transform sndBuilding;
+
+}
---
+++ Assets/_DefendTheFactory/Scripts/GridBuildingSystem.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GridBuildingSystem : MonoBehaviour {
+
+    public static GridBuildingSystem Instance { get; private set; }
+
+
+
+    public event EventHandler OnSelectedChanged;
+    public event EventHandler OnObjectPlaced;
+
+
+    private GridXZ<GridObject> grid;
+    private PlacedObjectTypeSO placedObjectTypeSO;
+    private PlacedObjectTypeSO.Dir dir;
+
+    private bool isDemolishActive;
+
+    private void Awake() {
+        Instance = this;
+
+        int gridWidth = 50;
+        int gridHeight = 50;
+        float cellSize = 1f;
+        grid = new GridXZ<GridObject>(gridWidth, gridHeight, cellSize, new Vector3(0, 0, 0), (GridXZ<GridObject> g, int x, int y) => new GridObject(g, x, y));
+
+        placedObjectTypeSO = null;
+    }
+
+    public class GridObject {
+
+        private GridXZ<GridObject> grid;
+        private int x;
+        private int y;
+        public PlacedObject placedObject;
+
+        public GridObject(GridXZ<GridObject> grid, int x, int y) {
+            this.grid = grid;
+            this.x = x;
+            this.y = y;
+            placedObject = null;
+        }
+
+        public override string ToString() {
+            return x + ", " + y + "\n" + placedObject;
+        }
+
+        public void TriggerGridObjectChanged() {
+            grid.TriggerGridObjectChanged(x, y);
+        }
+
+        public void SetPlacedObject(PlacedObject placedObject) {
+            this.placedObject = placedObject;
+            TriggerGridObjectChanged();
+        }
+
+        public void ClearPlacedObject() {
+            placedObject = null;
+            TriggerGridObjectChanged();
+        }
+
+        public PlacedObject GetPlacedObject() {
+            return placedObject;
+        }
+
+        public bool CanBuild() {
+            return placedObject == null;
+        }
+
+    }
+
+    private void Update() {
+        if (placedObjectTypeSO == GameAssets.i.placedObjectTypeSO_Refs.conveyorBelt) {
+            // Placing a belt, place in a line
+            HandleBeltPlacement();
+        } else {
+            HandleNormalObjectPlacement();
+        }
+        HandleDirRotation();
+        HandleDemolish();
+
+        if (Input.GetMouseButtonDown(1)) {
+            DeselectObjectType();
+        }
+    }
+
+    private void HandleNormalObjectPlacement() {
+        if (Input.GetMouseButtonDown(0) && placedObjectTypeSO != null && !UtilsClass.IsPointerOverUI()) {
+            Vector3 mousePosition = Mouse3D.GetMouseWorldPosition();
+            grid.GetXZ(mousePosition, out int x, out int z);
+
+            Vector2Int placedObjectOrigin = new Vector2Int(x, z);
+            if (TryPlaceObject(placedObjectOrigin, placedObjectTypeSO, dir, out PlacedObject placedObject)) {
+                // Object placed
+            } else {
+                // Error!
+                UtilsClass.CreateWorldTextPopup("Cannot Build Here!", mousePosition);
+            }
+        }
+    }
+
+    private void HandleBeltPlacement() {
+        // Placing a belt, place in a line
+        if (Input.GetMouseButton(0) && placedObjectTypeSO == GameAssets.i.placedObjectTypeSO_Refs.conveyorBelt && !UtilsClass.IsPointerOverUI()) {
+            Vector3 mousePosition = Mouse3D.GetMouseWorldPosition();
+            grid.GetXZ(mousePosition, out int x, out int z);
+
+            Vector2Int placedObjectOrigin = new Vector2Int(x, z);
+            TryPlaceObject(placedObjectOrigin, placedObjectTypeSO, dir, out PlacedObject placedObject);
+
+            // ###### TODO: PLACE BELTS IN A LINE
+            /*
+            if (beltPlaceStartGridPosition == null) {
+                // First stage, select start point
+                beltPlaceStartGridPosition = placedObjectOrigin;
+            } else {
+                // Second stage, place all belts
+                Vector2Int beltPlaceEndGridPosition = placedObjectOrigin;
+                for (int beltX = beltPlaceStartGridPosition.Value.x; beltX < beltPlaceEndGridPosition.x; beltX++) {
+                    TryPlaceObject(beltX, beltPlaceStartGridPosition.Value.y, placedObjectTypeSO, PlacedObjectTypeSO.Dir.Right);
+                }
+                for (int beltY = beltPlaceStartGridPosition.Value.y; beltY < beltPlaceEndGridPosition.y; beltY++) {
+                    TryPlaceObject(beltPlaceEndGridPosition.x, beltY, placedObjectTypeSO, PlacedObjectTypeSO.Dir.Up);
+
+                }
+            }
+            */
+        }
+    }
+
+    private void HandleDirRotation() {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            dir = PlacedObjectTypeSO.GetNextDir(dir);
+        }
+    }
+
+    private void HandleDemolish() {
+        if (isDemolishActive && Input.GetMouseButtonDown(0) && !UtilsClass.IsPointerOverUI()) {
+            Vector3 mousePosition = Mouse3D.GetMouseWorldPosition();
+            PlacedObject placedObject = grid.GetGridObject(mousePosition).GetPlacedObject();
+            if (placedObject != null) {
+                // Demolish
+                placedObject.DestroySelf();
+
+                List<Vector2Int> gridPositionList = placedObject.GetGridPositionList();
+                foreach (Vector2Int gridPosition in gridPositionList) {
+                    grid.GetGridObject(gridPosition.x, gridPosition.y).ClearPlacedObject();
+                }
+            }
+        }
+    }
+
+    private void UpdateCanBuildTilemap() {
+        for (int x = 0; x < grid.GetWidth(); x++) {
+            for (int y = 0; y < grid.GetHeight(); y++) {
+                // Tilemap
+                Tilemap.Instance.SetTilemapSprite(new Vector3(x, y),
+                    grid.GetGridObject(x, y).CanBuild() ? 
+                    Tilemap.TilemapObject.TilemapSprite.CanBuild :
+                    Tilemap.TilemapObject.TilemapSprite.CannotBuild);
+            }
+        }
+    }
+
+    public void DeselectObjectType() {
+        placedObjectTypeSO = null;
+        isDemolishActive = false;
+        RefreshSelectedObjectType();
+    }
+
+    private void RefreshSelectedObjectType() {
+        UpdateCanBuildTilemap();
+
+        if (placedObjectTypeSO == null) {
+            TilemapVisual.Instance.Hide();
+        } else {
+            TilemapVisual.Instance.Show();
+        }
+
+        OnSelectedChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public bool TryPlaceObject(int x, int y, PlacedObjectTypeSO placedObjectTypeSO, PlacedObjectTypeSO.Dir dir) {
+        return TryPlaceObject(new Vector2Int(x, y), placedObjectTypeSO, dir, out PlacedObject placedObject);
+    }
+
+    public bool TryPlaceObject(Vector2Int placedObjectOrigin, PlacedObjectTypeSO placedObjectTypeSO, PlacedObjectTypeSO.Dir dir) {
+        return TryPlaceObject(placedObjectOrigin, placedObjectTypeSO, dir, out PlacedObject placedObject);
+    }
+
+    public bool TryPlaceObject(Vector2Int placedObjectOrigin, PlacedObjectTypeSO placedObjectTypeSO, PlacedObjectTypeSO.Dir dir, out PlacedObject placedObject) {
+        // Test Can Build
+        List<Vector2Int> gridPositionList = placedObjectTypeSO.GetGridPositionList(placedObjectOrigin, dir);
+        bool canBuild = true;
+        foreach (Vector2Int gridPosition in gridPositionList) {
+            if (!grid.IsValidGridPositionWithPadding(gridPosition)) {
+                // Not valid
+                canBuild = false;
+                break;
+            }
+            if (!grid.GetGridObject(gridPosition.x, gridPosition.y).CanBuild()) {
+                canBuild = false;
+                break;
+            }
+        }
+
+        if (canBuild) {
+            Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
+            Vector3 placedObjectWorldPosition = grid.GetWorldPosition(placedObjectOrigin.x, placedObjectOrigin.y) + new Vector3(rotationOffset.x, 0, rotationOffset.y) * grid.GetCellSize();
+
+            placedObject = PlacedObject.Create(placedObjectWorldPosition, placedObjectOrigin, dir, placedObjectTypeSO);
+
+            foreach (Vector2Int gridPosition in gridPositionList) {
+                grid.GetGridObject(gridPosition.x, gridPosition.y).SetPlacedObject(placedObject);
+            }
+
+            placedObject.GridSetupDone();
+
+            OnObjectPlaced?.Invoke(placedObject, EventArgs.Empty);
+
+            return true;
+        } else {
+            // Cannot build here
+            placedObject = null;
+            return false;
+        }
+    }
+
+    public Vector2Int GetGridPosition(Vector3 worldPosition) {
+        grid.GetXZ(worldPosition, out int x, out int z);
+        return new Vector2Int(x, z);
+    }
+
+    public Vector3 GetWorldPosition(Vector2Int gridPosition) {
+        return grid.GetWorldPosition(gridPosition.x, gridPosition.y);
+    }
+
+    public GridObject GetGridObject(Vector2Int gridPosition) {
+        return grid.GetGridObject(gridPosition.x, gridPosition.y);
+    }
+
+    public GridObject GetGridObject(Vector3 worldPosition) {
+        return grid.GetGridObject(worldPosition);
+    }
+
+    public bool IsValidGridPosition(Vector2Int gridPosition) {
+        return grid.IsValidGridPosition(gridPosition);
+    }
+
+    public Vector3 GetMouseWorldSnappedPosition() {
+        Vector3 mousePosition = Mouse3D.GetMouseWorldPosition();
+        grid.GetXZ(mousePosition, out int x, out int z);
+
+        if (placedObjectTypeSO != null) {
+            Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
+            Vector3 placedObjectWorldPosition = grid.GetWorldPosition(x, z) + new Vector3(rotationOffset.x, 0, rotationOffset.y) * grid.GetCellSize();
+            return placedObjectWorldPosition;
+        } else {
+            return mousePosition;
+        }
+    }
+
+    public Quaternion GetPlacedObjectRotation() {
+        if (placedObjectTypeSO != null) {
+            return Quaternion.Euler(0, placedObjectTypeSO.GetRotationAngle(dir), 0);
+        } else {
+            return Quaternion.identity;
+        }
+    }
+
+    public PlacedObjectTypeSO GetPlacedObjectTypeSO() {
+        return placedObjectTypeSO;
+    }
+
+    public void SetSelectedPlacedObject(PlacedObjectTypeSO placedObjectTypeSO) {
+        this.placedObjectTypeSO = placedObjectTypeSO;
+        isDemolishActive = false;
+        RefreshSelectedObjectType();
+    }
+
+    public void SetDemolishActive() {
+        placedObjectTypeSO = null;
+        isDemolishActive = true;
+        RefreshSelectedObjectType();
+    }
+
+    public bool IsDemolishActive() {
+        return isDemolishActive;
+    }
+
+}
