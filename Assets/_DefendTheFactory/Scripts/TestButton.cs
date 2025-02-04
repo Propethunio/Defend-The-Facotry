00b2bebf0d271a91303ae8075ac339f05a14be8f
Propethunio <kuba.p.mazur@gmail.com>
2025-02-04 20:05:58 +0100

    test maps for 3Ds <3

--- Assets/_DefendTheFactory/Scripts/TestButton.cs
+++ Assets/_DefendTheFactory/Scripts/TestButton.cs
@@ -1,8 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TestButton : MonoBehaviour {
+
+    [SerializeField] PlacedObjectTypeSO obj;
+
+    void Start() {
+        TMP_Text text = GetComponentInChildren<TMP_Text>();
+        text.text = obj.nameString;
+    }
+
     public void SetItem(PlacedObjectTypeSO test) {
         BuildingSystem.Instance.Test(test);
     }
